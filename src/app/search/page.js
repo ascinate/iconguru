6a5b95fe248d8ce93e92d365b007b51c035b376d
samrat <samrat.subratadas@gmail.com>
2025-03-18 18:54:41 +0530

    last error solve

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -4,46 +4,9 @@ import { useSearchParams } from "next/navigation";
 import Head from "next/head";
 import NavicationSubpage from "../components/NavicationSubpage";
 import Footer from "../components/Footer";
-import SidebarFilter from "../components/SidebarFilter";
 
 export default function NewSearchResults() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("search") || "";
 
-  const [icons, setIcons] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    if (!query.trim()) return;
-
-    setLoading(true);
-    setIcons([]);
-    setError("");
-
-    fetch(`/api/icons/search?search=${encodeURIComponent(query)}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (data && Array.isArray(data.icons)) {
-          setIcons(data.icons);
-        } else {
-          setIcons([]);
-          setError("No icons found.");
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching search results:", error);
-        setError("Error fetching data. Check API or network.");
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [query]);
 
   return (
     <>
@@ -54,33 +17,7 @@ export default function NewSearchResults() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavicationSubpage />
-      <main className="conatiner-fluid">
-         <div className="row">
-              <div className="col-lg-3">
-                  <SidebarFilter/>
-              </div>
-              <div className="col-lg-9">
-                 <h2>Results for "{query}"</h2>
-                  {error && <p className="text-danger">{error}</p>}
-                  <div className="row row-cols-1 row-cols-lg-6 gy-4 g-lg-4">
-                    {loading ? (
-                      <p>Loading...</p>
-                    ) : icons.length > 0 ? (
-                      icons.map((icon) => (
-                        <div key={icon.Id} className="svg-item col">
-                          <span dangerouslySetInnerHTML={{ __html: icon.icon_svg }}></span>
-                          <p>{icon.icon_name}</p>
-                        </div>
-                      ))
-                    ) : (
-                      <p>No icons found.</p>
-                    )}
-                  </div>
-              </div>
-         </div>
-         
-        
-      </main>
+    
       <Footer />
     </>
   );
