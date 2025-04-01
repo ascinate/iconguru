9813919437e8911f624c598245e4a6bc66e0958f
samrat <samrat.subratadas@gmail.com>
2025-04-01 19:48:06 +0530

    register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,11 +1,22 @@
 
 "use client";
 import { useState } from "react";
+import NavicationHome from "../components/NavicationHome";
+import Link from "next/link";
+import Form from "next/form";
+import Image from "next/image";
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
+
+  const [isVisible, setIsVisible] = useState(true);
+  const [hiddend, setHiddend] = useState(false);
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible); 
+    setHiddend(!hiddend); 
+  };
 
   const handleRegister = async (e) => {
     e.preventDefault();
@@ -34,27 +45,86 @@ export default function Register() {
   };
 
   return (
-    <div className="container">
+    <>
+    <NavicationHome/>
+    <main className="float-start w-100 register-pages-crm">
+        <div className="container">
+            <h2 className="text-center"> Continue with Email </h2>
+            <p className="text-center"> Already have an account? <Link href='/login'> <strong> Log in </strong> </Link> </p>
+            
+            <div className="col-lg-8 mx-auto">
+                <button className="btn btn-comon-socail d-flex align-items-center justify-content-center"> 
+                  <span> 
+                    <Image loading="lazy" src="/logo_googleg_48dp.svg"
+                                          alt="iconsguru"
+                                          width={24}
+                                          height={24} /> 
+                  </span>  Continue with Google 
+                </button>
+            </div>
+            <div className="comon-rgesigner-sections d-inline-block w-100"></div>
+
+            <Form onSubmit={handleRegister}>
+              <input
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <input
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <button type="submit">Register</button>
+            </Form>
+            {message && <p>{message}</p>}
+
+        </div>
+    </main>
+    <div className="container d-none">
       <h1>Register</h1>
-      <form onSubmit={handleRegister}>
-        <input
-          type="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-         <input
-          type="password"
-          placeholder="Enter password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Register</button>
-      </form>
-      {message && <p>{message}</p>}
+
+      <button
+        onClick={toggleVisibility} 
+        style={{ padding: '10px 20px', fontSize: '16px', cursor: 'pointer' }}
+      >
+        {isVisible ? 'Hide' : 'Show'} Div
+      </button>
+
+
+      {isVisible && (
+        <div
+          style={{
+            marginTop: '20px',
+            padding: '20px',
+            backgroundColor: 'lightblue',
+            borderRadius: '8px',
+          }}
+        >
+          This is a toggled div!
+        </div>
+        )}
+
+
+        {hiddend && (
+        <div
+          style={{
+            marginTop: '20px',
+            padding: '20px',
+            backgroundColor: 'lightblue',
+            borderRadius: '8px',
+          }}
+        >
+          This is a toggled div show!
+        </div>
+        )}
+      
     </div>
+    </>
 
   );
 }
